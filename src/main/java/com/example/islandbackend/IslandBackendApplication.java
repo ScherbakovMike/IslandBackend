8a9f654f49d7d3033a21d66d3e0767dd917f4560
mike <scherbakov.mike@gmal.com>
2022-05-12 20:51:31 +0300

    Initial commit

--- src/main/java/com/example/islandbackend/IslandBackendApplication.java
+++ src/main/java/com/example/islandbackend/IslandBackendApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 class IslandBackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(IslandBackendApplication.class, args);
     }
